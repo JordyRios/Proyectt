503c0289aa86951107aa18106e62d035bee04d90
JordyRios <119907843+JordyRios@users.noreply.github.com>
2024-05-23 14:44:20 -0600

    php.php

---
+++ phop.php
@@ -0,0 +1,39 @@
+<?php
+// Configuración de la conexión a la base de datos
+$host = "adminuser1.mysql.database.azure.com";
+$username = "adminuser";
+$password = "Chuchaq12*";
+$database = "adminuser1";
+$port = 3306; // Puerto predeterminado para MySQL
+
+// Inicializar una nueva instancia de conexión mysqli
+$con = mysqli_init();
+
+// Configurar la seguridad SSL
+mysqli_ssl_set($con, NULL, NULL, "{path_to_CA_cert}", NULL, NULL);
+
+// Establecer la conexión a la base de datos utilizando SSL
+if (!mysqli_real_connect($con, $host, $username, $password, $database, $port, MYSQLI_CLIENT_SSL)) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
+// Verificar si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtener los datos del formulario y escaparlos para prevenir inyección SQL
+    $nombre = mysqli_real_escape_string($con, $_POST['nombre']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+
+    // Crear la consulta SQL para insertar los datos en la base de datos
+    $sql = "INSERT INTO nombre_de_la_tabla (nombre, email) VALUES ('$nombre', '$email')";
+
+    // Ejecutar la consulta y verificar si fue exitosa
+    if (mysqli_query($con, $sql)) {
+        echo "<h2>Datos insertados correctamente</h2>";
+    } else {
+        echo "Error al ejecutar la consulta: " . mysqli_error($con);
+    }
+}
+
+// Cerrar la conexión
+mysqli_close($con);
+?>
