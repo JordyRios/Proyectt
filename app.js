5af7803b5053924bc605d7c566b4f9228626badf
JordyRios <119907843+JordyRios@users.noreply.github.com>
2024-05-24 17:53:56 -0600

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: 'adminuser2.mysql.database.azure.com',
     user: 'adminuser2',
-    password: 'Chucha@12*', // reemplaza con tu contraseña de administrador
-    database: 'adminuser2', // reemplaza con el nombre de tu base de datos
+    password: 'Chucha@12*', 
+    database: 'adminuser2', 
     ssl: {
         rejectUnauthorized: false
     }
